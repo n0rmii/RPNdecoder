4c93c8b004797f8dcff6bb1b2cb5913029b6bc61
n0rmii <153909826+n0rmii@users.noreply.github.com>
2024-01-26 10:28:33 +0100

    dziaua

--- RPNdecoder.cpp
+++ RPNdecoder.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <string>
 #include "stackWithAccess.hpp"
 
 int main(int argc, char *argv[]){
-	stack instructionStack;
-	instructionStack.push(7);
-	std::cout<<instructionStack.pop();
+	stack dataStack;
+	bool running = true;
+	std::string input;
+	int tmpVal;
+	while(running){
+		std::cout<<"Input: ";
+		std::getline(std::cin, input);
+		for (char s : input) {
+			switch (s) {
+				case 'e':
+					running = false;
+					break;
+				case '+':
+					dataStack.push(dataStack.pop() + dataStack.pop());
+					break;
+				case '-':
+					tmpVal = dataStack.pop(); // I need to switch the positions of pops
+					dataStack.push(dataStack.pop() - tmpVal);
+					break;
+				case '*':
+					dataStack.push(dataStack.pop() * dataStack.pop());
+					break;
+				case '/':
+					tmpVal = dataStack.pop(); // I need to switch the positions of pops
+					dataStack.push(dataStack.pop() / tmpVal);
+					break;
+				case ' ':
+					break;
+				default:
+					dataStack.push(s - '0');
+			}
+		}
+		std::cout<<"Output: "<<dataStack.pop()<<std::endl;
+	}
 	return 0;
 }
